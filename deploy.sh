68d7e1fff95bebe95630cb5da718d29da5009f2d
DAVID-LAPTOP\david <davidgl1987@gmail.com>
2023-02-06 08:37:02 +0100

    Change CNAME

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ cd dist
 echo > .nojekyll
 
 # if you are deploying to a custom domain
-echo 'https://davidgl1987.github.io/podcaster/' > CNAME
+# echo 'www.example.com' > CNAME
 
 git init
 git checkout -B main
